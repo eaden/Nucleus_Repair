adb347b23862331beebc0b525d3816ba059d584d
K K <kmk@gmx.de>
2020-01-31 22:05:12 +0100

    Nucleus movement with arrows. Black sprite for nucleus

---
+++ Assets/NucleusScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleusScript : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2();
+        if (Input.GetKey(KeyCode.UpArrow))
+            rigid.velocity = new Vector2(rigid.velocity.x, speed);
+        if (Input.GetKey(KeyCode.DownArrow))
+            rigid.velocity = new Vector2(rigid.velocity.x, -speed);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rigid.velocity = new Vector2(-speed, rigid.velocity.y);
+        if (Input.GetKey(KeyCode.RightArrow))
+            rigid.velocity = new Vector2(speed, rigid.velocity.y);
+    }
+}
